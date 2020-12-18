421a846c45f2bf82557843c906357c3887062760
Hzy0913 <hanzhaoy@chanjet.com>
2020-12-18 13:40:43 +0800

    style: update code style

--- src/components/swipe/index.vue
+++ src/components/swipe/index.vue
@@ -225,53 +225,52 @@
           }
 
           element.style.left = (( (to - from) * (Math.floor((timeElap / speed) * 100) / 100) ) + from) + 'px';
-
         }, 4);
-
       }
 
       // setup initial vars
-      var start = {};
-      var delta = {};
-      var isScrolling;
+      let start = {};
+      let delta = {};
+      let isScrolling;
 
       // setup event capturing
-      var events = {
-
-        handleEvent: function(event) {
-
+      const events = {
+        handleEvent(event) {
           switch (event.type) {
-            case 'touchstart': this.start(event); break;
-            case 'touchmove': this.move(event); break;
-            case 'touchend': offloadFn(this.end(event)); break;
+            case 'touchstart':
+              this.start(event);
+              break;
+            case 'touchmove':
+              this.move(event);
+              break;
+            case 'touchend':
+              offloadFn(this.end(event));
+              break;
             case 'webkitTransitionEnd':
             case 'msTransitionEnd':
             case 'oTransitionEnd':
             case 'otransitionend':
-            case 'transitionend': offloadFn(this.transitionEnd(event)); break;
-            case 'resize': offloadFn(setup); break;
+            case 'transitionend':
+              offloadFn(this.transitionEnd(event));
+              break;
+            case 'resize':
+              offloadFn(setup);
+              break;
           }
 
           if (options.stopPropagation) {
             event.stopPropagation();
             event.preventDefault();
           }
-
         },
-        start: function(event) {
-
-          var touches = event.touches[0];
-
+        start(event) {
+          const touches = event.touches[0];
           // measure start values
           start = {
-
             // get initial touch coords
             x: touches.pageX,
             y: touches.pageY,
-
-            // store time to determine touch duration
-            time: +new Date
-
+            time: +new Date // store time to determine touch duration
           };
 
           // used for testing first move event
@@ -281,19 +280,16 @@
           delta = {};
 
           // attach touchmove and touchend listeners
-          console.log(element, 12312312)
           element.addEventListener('touchmove', this, false);
           element.addEventListener('touchend', this, false);
-
         },
-        move: function(event) {
-
+        move(event) {
           // ensure swiping with one touch and not pinching
-          if ( event.touches.length > 1 || event.scale && event.scale !== 1) return
+          if (event.touches.length > 1 || event.scale && event.scale !== 1) return
 
           if (options.disableScroll) event.preventDefault();
 
-          var touches = event.touches[0];
+          const touches = event.touches[0];
 
           // measure change in x and y
           delta = {
@@ -302,13 +298,12 @@
           }
 
           // determine if scrolling test has run - one time test
-          if ( typeof isScrolling == 'undefined') {
+          if (typeof isScrolling == 'undefined') {
             isScrolling = !!( isScrolling || Math.abs(delta.x) < Math.abs(delta.y) );
           }
 
           // if user is not trying to scroll vertically
           if (!isScrolling) {
-
             // prevent native scrolling
             event.preventDefault();
 
@@ -323,7 +318,6 @@
               translate(circle(index+1), delta.x + slidePos[circle(index+1)], 0);
 
             } else {
-
               delta.x =
                 delta.x /
                 ( (!index && delta.x > 0               // if first slide and sliding left
@@ -338,43 +332,37 @@
               translate(index, delta.x + slidePos[index], 0);
               translate(index+1, delta.x + slidePos[index+1], 0);
             }
-
           }
-
         },
-        end: function(event) {
-
+        end(event) {
           // measure duration
-          var duration = +new Date - start.time;
+          const duration = +new Date - start.time;
 
           // determine if slide attempt triggers next/prev slide
-          var isValidSlide =
+          const isValidSlide =
             Number(duration) < 250               // if slide duration is less than 250ms
             && Math.abs(delta.x) > 20            // and if slide amt is greater than 20px
             || Math.abs(delta.x) > width/2;      // or if slide amt is greater than half the width
 
           // determine if slide attempt is past start and end
-          var isPastBounds =
+          let isPastBounds =
             !index && delta.x > 0                            // if first slide and slide amt is greater than 0
             || index == slides.length - 1 && delta.x < 0;    // or if last slide and slide amt is less than 0
 
-          if (options.continuous) isPastBounds = false;
+          if (options.continuous) {
+            isPastBounds = false;
+          }
 
           // determine direction of swipe (true:right, false:left)
-          var direction = delta.x < 0;
+          const direction = delta.x < 0;
 
           // if not scrolling vertically
           if (!isScrolling) {
-
             if (isValidSlide && !isPastBounds) {
-
               if (direction) {
-
                 if (options.continuous) { // we need to get the next in this direction in place
-
                   move(circle(index-1), -width, 0);
                   move(circle(index+2), width, 0);
-
                 } else {
                   move(index-1, -width, 0);
                 }
@@ -385,10 +373,8 @@
 
               } else {
                 if (options.continuous) { // we need to get the next in this direction in place
-
                   move(circle(index+1), width, 0);
                   move(circle(index-2), -width, 0);
-
                 } else {
                   move(index+1, width, 0);
                 }
@@ -396,28 +382,20 @@
                 move(index, slidePos[index]+width, speed);
                 move(circle(index-1), slidePos[circle(index-1)]+width, speed);
                 index = circle(index-1);
-
               }
 
               options.callback && options.callback(index, slides[index]);
-
             } else {
-
               if (options.continuous) {
-
                 move(circle(index-1), -width, speed);
                 move(index, 0, speed);
                 move(circle(index+1), width, speed);
-
               } else {
-
                 move(index-1, -width, speed);
                 move(index, 0, speed);
                 move(index+1, width, speed);
               }
-
             }
-
           }
 
           // kill touchmove and touchend event listeners until touchstart called again
@@ -425,30 +403,22 @@
           element.removeEventListener('touchend', events, false)
 
         },
-        transitionEnd: function(event) {
-
-          if (parseInt(event.target.getAttribute('data-index'), 10) == index) {
-
-
+        transitionEnd(event) {
+          if (parseInt(event.target.getAttribute('data-index'), 10) === index) {
             options.transitionEnd && options.transitionEnd.call(event, index, slides[index]);
-
           }
-
         }
-
       }
-
-
 
       onMounted(() => {
         container = swipeRef.value;
         setup();
 
-
         if (browser.addEventListener) {
-
           // set touchstart event on element
-          if (browser.touch) element.addEventListener('touchstart', events, false);
+          if (browser.touch) {
+            element.addEventListener('touchstart', events, false);
+          }
 
           if (browser.transitions) {
             element.addEventListener('webkitTransitionEnd', events, false);
@@ -460,18 +430,16 @@
 
           // set resize event on window
           window.addEventListener('resize', events, false);
-
         } else {
-
-          window.onresize = function () { setup() }; // to play nice with old IE
-
+          window.onresize = function () {  // to play nice with old IE
+            setup()
+          };
         }
         console.dir(swipeRef);
       })
 
       return {
         swipeRef,
-
       }
     }
   }
