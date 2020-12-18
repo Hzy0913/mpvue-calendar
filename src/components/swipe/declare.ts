880d3187283997efcc695de5ca1cc6bf9f36a8e8
Hzy0913 <hanzhaoy@chanjet.com>
2020-12-18 14:26:08 +0800

    feat: add declare

---
+++ src/components/swipe/declare.ts
@@ -0,0 +1,20 @@
+interface SwipeInterface {
+
+}
+
+type startType = {
+  x: number;
+  y: number;
+  time: number;
+}
+
+type deltaType = {
+  x: number;
+  y: number;
+}
+
+export {
+  SwipeInterface,
+  startType,
+  deltaType,
+}
