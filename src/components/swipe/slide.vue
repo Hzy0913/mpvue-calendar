7369460bac15f38c2178c38315ebc6abf1211a83
Hzy0913 <hanzhaoy@chanjet.com>
2020-12-21 09:50:56 +0800

    feat: add slide component

---
+++ src/components/swipe/slide.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="swipe-slide" :class="{[className]: !!className}">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, reactive, onMounted } from 'vue'
+  import { SlideInterface } from './declare'
+  import './style.less'
+
+  export default {
+    props: {
+      className: {
+        type: String,
+        default: ''
+      },
+    },
+    setup(props: SlideInterface) {
+      const { className } = props;
+
+      return {
+        className,
+      }
+    }
+  }
+</script>
