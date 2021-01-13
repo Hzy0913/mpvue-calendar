00f93c6afd85ef2e1db41296e9657400501a61e9
Hzy0913 <hanzhaoy@chanjet.com>
2021-01-13 15:17:00 +0800

    feat: update date2timeStamp & range multi mode

--- src/components/timetable/computed.ts
+++ src/components/timetable/computed.ts
@@ -9,6 +9,11 @@ function date2ymd(date: string): number[] {
   return [Number(y), Number(m), Number(d)];
 }
 
+function date2timeStamp(date: string): number {
+  const [y, m, d] = date2ymd(date);
+  return +new Date(y, m, d);
+}
+
 function getLunarInfo(y: string, m: string, d: string) {
   const lunarInfo = calendar.solar2lunar(y, m, d) as any;
   const {Term, lMonth, lDay, lYear} = lunarInfo || {};
@@ -102,111 +107,75 @@ type rangeOptionType = {
 function isCurrentMonthToday(date: string) {
   const todayString = getToday();
   return todayString === date;
-  // if (!isToday) return false;
-  // return weekMode ? isToday : (Number(todayString.split('-')[1]) === month + 1);
 }
 
-const rangeOption = function({date, isWeekMode, rangeDate, playload, weekSwitch, getEvents, getLunarInfo, disabledDateHandle}: any) {
-  const [year, month, day] = date2ymd(date);
-  const lastDay = new Date(year, month - 1, 0).getDate() === day ? {lastDay: true} : null;
-  const options = Object.assign(
-    {day},
-    getLunarInfo(year, month, day),
-    getEvents(year, month, day),
-    lastDay
-  );
-  const [rangeBegin, rangeEnd] = rangeDate || [];
-  const [beginY, beginM, beginD] = rangeBegin.split('-').map(Number);
-  const [endY, endM, endD] = rangeEnd.split('-').map(Number);
-  const isMonthModeCurrentMonth = !weekSwitch && !playload;
-
-  if (isWeekMode || isMonthModeCurrentMonth) {
-    (rangeBegin === date) && (options.rangeClassName = 'mc-range-begin');
-    (rangeEnd === date) && (options.rangeClassName = 'mc-range-end');
+const rangeOption = function({selectDate, date}: any) {
+  const { start, end } = selectDate;
+  if (start === date) {
+    return 'selected selected-range-start'
+  }
+  if (end === date) {
+    return 'selected selected-range-end'
   }
 
-  if (year === endY && month === endM && day === endD) {
-    options.rangeClassName = options.rangeClassName ? ['mc-range-begin', 'mc-range-second-to-last'] : 'mc-range-second-to-last';
-  }
-
-  if (rangeBegin) {
-    const beginStamp = +new Date(beginY, beginM - 1, beginD);
-    const endStamp = +new Date(endY, endM - 1, endD);
-    const currentStamp = +new Date(year, month - 1, day);
-    if (beginStamp <= currentStamp && endStamp >= currentStamp) {
-      options.selected = true;
+  if (start && end && date) {
+    const startTimeStamp: number = date2timeStamp(start);
+    const endTimeStamp: number = date2timeStamp(end);
+    const currentTimeStamp: number = date2timeStamp(date);
+    if (startTimeStamp < currentTimeStamp && currentTimeStamp < endTimeStamp) {
+      return 'selected selected-range-includes'
     }
   }
-
-  if (rangeEnd) {
-    const beginStamp = +new Date(beginY, beginM - 1, beginD);
-    if (beginStamp > +new Date(year, month, day)) {
-      options.disabled = true;
-    }
-  }
-
-  if (playload && !weekSwitch) {
-    options.disabled = true;
-  } else if (disabledDateHandle.isDisabled(date)) {
-    options.disabled = true;
-  }
-
-  const monthFirstDay = `${year}-${month}-1`;
-  const monthLastDay = `${year}-${month}-${new Date(year, month, 0).getDate()}`;
-  if (monthFirstDay === date && options.selected && !options.rangeClassName) {
-    options.rangeClassName = 'mc-range-month-first';
-  }
-  if (monthLastDay === date && options.selected && !options.rangeClassName) {
-    options.rangeClassName = 'mc-range-month-last';
-  }
-
-  isCurrentMonthToday(date) && (options.isToday = true);
-  (!weekSwitch && playload) && (options.selected = false);
-  return options;
 }
 
-const multiOption = function({value, date, begin, end, isWeekMode, rangeDate, playload, weekSwitch, getEvents, getLunarInfo, disabledDateHandle}: any) {
-  const [year, month, day] = date2ymd(date);
-  const [beginY, beginM, beginD] = begin.split('-').map(Number);
-  const [endY, endM, endD] = end.split('-').map(Number);
+const multiOption = function({selectDate, date, begin, end, isWeekMode, rangeDate, playload, weekSwitch, getEvents, getLunarInfo, disabledDateHandle}: any) {
+  console.log(selectDate,date, 111 )
 
-  const isSelected = value.includes(date);
-  let options = Object.assign(
-    {day, selected: isSelected},
-    getLunarInfo(year, month, day),
-    getEvents(year, month, day),
-  );
+  return selectDate.includes(date) ? 'selected' : undefined;
 
-  if (isSelected) {
-    if (begin) {
-      const beginStamp = +new Date(beginY, beginM - 1, beginD);
-      if (beginStamp > +new Date(year, month - 1, day)) {
-        options.disabled = true;
-      }
-    }
-
-    if (end) {
-      const endStamp = +new Date(endY, endM - 1, endD);
-      if (endStamp < +new Date(year, month, day)) {
-        options.disabled = true;
-      }
-    }
-
-    if (disabledDateHandle.isDisabled(date)) {
-      options.disabled = true;
-    }
-  }
-
-  isCurrentMonthToday(options) && (options.isToday = true);
-
-  if (playload && !weekSwitch) {
-    options.disabled = true;
-    options.selected = false;
-  }
-
-  return options;
+  // const [year, month, day] = date2ymd(date);
+  // const [beginY, beginM, beginD] = begin.split('-').map(Number);
+  // const [endY, endM, endD] = end.split('-').map(Number);
+  //
+  // const isSelected = value.includes(date);
+  // let options = Object.assign(
+  //   {day, selected: isSelected},
+  //   getLunarInfo(year, month, day),
+  //   getEvents(year, month, day),
+  // );
+  //
+  // if (isSelected) {
+  //   if (begin) {
+  //     const beginStamp = +new Date(beginY, beginM - 1, beginD);
+  //     if (beginStamp > +new Date(year, month - 1, day)) {
+  //       options.disabled = true;
+  //     }
+  //   }
+  //
+  //   if (end) {
+  //     const endStamp = +new Date(endY, endM - 1, endD);
+  //     if (endStamp < +new Date(year, month, day)) {
+  //       options.disabled = true;
+  //     }
+  //   }
+  //
+  //   if (disabledDateHandle.isDisabled(date)) {
+  //     options.disabled = true;
+  //   }
+  // }
+  //
+  // isCurrentMonthToday(options) && (options.isToday = true);
+  //
+  // if (playload && !weekSwitch) {
+  //   options.disabled = true;
+  //   options.selected = false;
+  // }
+  //
+  // return options;
 }
-
+function selectOption({date, selectDate}: any) {
+  return selectDate === date ? 'selected' : undefined;
+}
 type selectOptionType = {
   date: string;
   playload: any;
@@ -216,45 +185,7 @@ type selectOptionType = {
   getLunarInfo: (year: number, month: number, day: number) => any;
   getEvents: (year: number, month: number, day: number) => any;
 }
-function selectOption({year, month, i, selectDate, isWeekMode, playload, begin,end, weekSwitch, getRemark, getTileContent, disabledDateHandle}: any) {
-  const date = `${year}-${month}-${i}`;
-  const dateTimestamp = +new Date(year, month - 1, i);
 
-  const options: any = Object.assign(
-    {day: i, selected: selectDate === date},
-    getLunarInfo(year, month, i),
-    getRemark(date),
-    getTileContent(date),
-  );
-
-  if (begin) {
-    const [beginY, beginM, beginD] = begin.split('-');
-    const beginTimestamp = +new Date(beginY, beginM - 1, beginD);
-    if (beginTimestamp > dateTimestamp) {
-      options.disabled = true;
-    }
-  }
-
-  if (end) {
-    const [endY, endM, endD] = end.split('-');
-    const endTimestamp = +new Date(endY, endM - 1, endD);
-    if (endTimestamp < dateTimestamp) {
-      options.disabled = true;
-    }
-  }
-
-  if (disabledDateHandle.isDisabled(date)) {
-    options.disabled = true;
-  }
-
-    isCurrentMonthToday(options) && (options.isToday = true);
-    //
-    // if (playload && !weekSwitch) {
-    //   options.disabled = true;
-    //   options.selected = false;
-    // }
-    return options;
-}
 
 const disabledDate = (function() {
   let disabledDate: any = {};
@@ -300,8 +231,11 @@ export {
   disabledDate,
   setRemark,
   setTileContent,
+  getLunarInfo,
   computedPrevYear,
   computedPrevMonth,
   computedNextYear,
-  computedNextMonth
+  computedNextMonth,
+  isCurrentMonthToday,
+  date2timeStamp,
 }
