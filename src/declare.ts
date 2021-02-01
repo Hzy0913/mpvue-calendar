4dbf1ada0342a3054ba87ea53b12f713368d5bdd
Hzy0913 <hanzhaoy@chanjet.com>
2021-02-01 16:56:12 +0800

    feat: update declare for calendar props

--- src/declare.ts
+++ src/declare.ts
@@ -1,6 +1,20 @@
 interface CalendarInterface {
   monFirst?: boolean;
+  completion?: boolean;
+  useSwipe?: boolean;
   weeks?: string[];
+  holidays: {[key: string]: string};
+  tileContent: {[key: string]: any };
+  remarks: {[key: string]: any };
+  disabled: string[];
+  begin: string[];
+  end: string[];
+  monthRange: string[];
+  mode: 'monthRange' | 'week' | 'month';
+  selectMode: 'select' | 'multi' | 'range' | 'multiRange';
+  lunar: any;
+  selectDate: any;
+  format: (year: string, month: string) => string[];
 }
 
 export {
